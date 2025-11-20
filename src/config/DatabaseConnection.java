5911cec37a9591ff19b3b66283f88e1b4e34cb6c
Lara Hernández <larahernandez.edu@gmail.com>
2025-11-20 18:28:59 -0300

    Fix de propiedades del proyecto para hacer el test y fix de la conexion del BD

--- src/config/DatabaseConnection.java
+++ src/config/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     
-    private static final String URL = "jdbc:mysql://localhost:3306/mascotas_microchips";
+    private static final String URL = "jdbc:mysql://localhost:3306/pets_microchips";
     private static final String USER = "root";
     private static final String PASSWORD = "";
     
